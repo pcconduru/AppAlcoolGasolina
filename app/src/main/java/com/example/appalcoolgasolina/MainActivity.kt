f4211a7a8c6a234964751b8a6421df904567d42e
Paulo César Conduru <133069658+pcconduru@users.noreply.github.com>
2025-01-16 03:20:10 -0300

    Atualizando o Código do Projeto
    Foi atualizado o código do projeto trazendo funcionalidades pro mesmo e melhorando alguns componentes de interface

--- app/src/main/java/com/example/appalcoolgasolina/MainActivity.kt
+++ app/src/main/java/com/example/appalcoolgasolina/MainActivity.kt
@@ -1,12 +1,25 @@
 package com.example.appalcoolgasolina
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var textInputAlcool: TextInputLayout
+    private lateinit var editAlcool: TextInputEditText
+
+    private lateinit var textInputGasolina: TextInputLayout
+    private lateinit var editGasolina: TextInputEditText
+
+    private lateinit var btnCalcular: Button
+    private lateinit var textResultado: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +29,58 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        inicializarComponentesInterface()
+        btnCalcular.setOnClickListener {
+            calcularMelhorPreco()
+        }
+    }
+
+    private fun calcularMelhorPreco(){
+        val precoAlcool = editAlcool.text.toString()
+        val precoGasolina = editGasolina.text.toString()
+
+        val resultadoValidacao = validarCampos(precoAlcool, precoGasolina)
+        if ( resultadoValidacao ){
+
+            val precoAlcoolNumero = precoAlcool.toDouble()
+            val precoGasolinaNumero = precoGasolina.toDouble()
+
+            val resultado = precoAlcoolNumero / precoGasolinaNumero
+
+            if ( resultado >= 0.7) {
+                textResultado.setText("Melhor Escolher Gasolina!")
+            }
+            else {
+                textResultado.setText("Melhor Escolher Álcool!")
+            }
+        }
+
+    }
+
+    private fun validarCampos(precoAlcool: String, precoGasolina: String): Boolean {
+
+        textInputAlcool.error = null
+        textInputGasolina.error = null
+
+        if ( precoAlcool.isEmpty() ){
+            textInputAlcool.error = "Digite o valor do Álcool"
+            return false
+        }
+        else if ( precoGasolina.isEmpty() ){
+            textInputGasolina.error = "Digite o valor da Gasolina"
+            return false
+        }
+        return true
+    }
+
+    private fun inicializarComponentesInterface() {
+        textInputAlcool = findViewById(R.id.text_input_alcool)
+        editAlcool = findViewById(R.id.edit_alcool)
+
+        textInputGasolina = findViewById(R.id.text_input_gasolina)
+        editGasolina = findViewById(R.id.edit_gasolina)
+
+        btnCalcular = findViewById(R.id.btn_calcular)
+        textResultado = findViewById(R.id.text_resultado)
     }
 }
